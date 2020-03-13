2c3bf84f930e25d52da101ac0bfb181a7fa18363
Ajay Sridhar <ajaysrid708@gmail.com>
2020-03-13 12:46:24 -0700

    Change shape of NPCs from rectangles to circles

--- covid19_game.py
+++ covid19_game.py
@@ -6,8 +6,8 @@ import math
 # dimensions
 display_width = 1200
 display_height = 1200
-person_width = 15
-person_height = 20
+'''person_width = 15
+person_height = 20'''
 population = 200
 
 # color definitions
@@ -43,36 +43,43 @@ class Vector:
         self.x /= self.v
         self.y /= self.v
 
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
 
 class Patient:
-    def __init__(self, pos, infected):
+    def __init__(self, pos, radius, infected):
         self.pos = pos
+        self.radius = radius
         self.infected = infected
 
-    def is_hit(self, axis, n):
-        if axis == 'x':
+    def is_hit(self, pos, radius):
+        '''if axis == 'x':
             if self.pos.x <= n <= self.pos.x + person_width:
                 return True
         elif axis == 'y':
             if self.pos.y <= n <= self.pos.y + person_height:
-                return True
+                return True'''
+        if (self.pos - pos).v < self.radius + radius:
+            return True
         return False
 
     def out_of_bounds(self):
-        if self.is_hit('x', 0) or self.is_hit('x', display_width) or self.is_hit('y', 0) or self.is_hit('y', display_height):
+        '''if self.is_hit('x', 0) or self.is_hit('x', display_width) or self.is_hit('y', 0) or self.is_hit('y', display_height):'''
+        if self.pos.x < self.radius or self.pos.x > display_width - self.radius or self.pos.y < self.radius or self.pos.y > display_height - self.radius:
             return True
         return False
 
     def is_infected(self):
         for infected_patient in infected_people:
-            if (self.is_hit('x', infected_patient.pos.x) or
-                    self.is_hit('x', infected_patient.pos.x + person_width)):
-                if (self.is_hit('y', infected_patient.pos.y) or
-                        self.is_hit('y', infected_patient.pos.y + person_height)):
-                    self.infected = True
-                    people.remove(self)
-                    infected_people.append(self)
-                    return True
+            if self.is_hit(infected_patient.pos, infected_patient.radius):
+                self.infected = True
+                people.remove(self)
+                infected_people.append(self)
+                return True
         return False
 
     def move(self, dx, dy):
@@ -84,18 +91,19 @@ class Patient:
             color = infected_color
         else:
             color = healthy_color
-        pygame.draw.rect(game_display, color,
-                         [self.pos.x, self.pos.y, person_width, person_height])
+        pygame.draw.circle(game_display, color,
+                           (int(self.pos.x), int(self.pos.y)), self.radius)
 
 
 # TODO: NPC and Player inherits from the player superclass
 class Npc(Patient):
-    def __init__(self, infected):
-        pos = Vector(
-            random.randrange(0, display_width - person_width),
-            random.randrange(0, display_height - person_height)
+    def __init__(self, radius, infected):
+
+        Patient.__init__(self, Vector(0, 0), radius, infected)
+        self.pos = Vector(
+            random.randrange(self.radius, display_width - self.radius),
+            random.randrange(self.radius, display_height - self.radius)
         )
-        Patient.__init__(self, pos, infected)
         self.vel = Vector(random.randrange(-200, 200),
                           random.randrange(-200, 200))
         self.vel.normalize()
@@ -104,9 +112,9 @@ class Npc(Patient):
         self.vel.y *= scal
 
     def bounce(self):
-        if self.is_hit('x', 0) or self.is_hit('x', display_width):
+        if self.pos.x < self.radius or self.pos.x > display_width - self.radius:
             self.vel.x = -self.vel.x
-        if self.is_hit('y', 0) or self.is_hit('y', display_height):
+        if self.pos.y < self.radius or self.pos.y > display_height - self.radius:
             self.vel.y = -self.vel.y
 
     def animate(self):
@@ -117,7 +125,7 @@ class Npc(Patient):
         self.draw()
 
 
-class Player(Patient):
+'''class Player(Patient):
     def __init__(self, pos, controls, infected):
         Patient.__init__(self, pos, infected)
         self.controls = controls
@@ -135,23 +143,25 @@ class Player(Patient):
         if key_pressed[self.controls[3]] and self.pos.y < display_height - person_height:
             self.move(0, 2)
         self.draw(infected_color=yellow, healthy_color=teal)
-
+'''
 
 # functions
+
+
 def quit_game():
     pygame.quit()
     quit()
 
 
 def game_loop():
-    time.sleep(3)
+    # time.sleep(3)
     for i in range(0, population):
-        people.append(Npc(False))
+        people.append(Npc(10, False))
 
-    player = Player(Vector(display_width/2 - person_width/2, display_height/2 - person_height/2), [pygame.K_LEFT, pygame.K_RIGHT,
+    '''player = Player(Vector(display_width/2 - person_width/2, display_height/2 - person_height/2), [pygame.K_LEFT, pygame.K_RIGHT,
                                                                                                    pygame.K_UP, pygame.K_DOWN], False)
-    people.append(player)
-    infected_people.append(Npc(True))
+    people.append(player)'''
+    infected_people.append(Npc(10, True))
 
     game_exit = False
     while not game_exit:
