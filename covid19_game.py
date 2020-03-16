7dbd92d415ca45033f95efeafda5187a1fbfa902
Ajay Sridhar <ajaysrid708@gmail.com>
2020-03-15 17:09:53 -0700

    Fix virus transmission animation

--- covid19_game.py
+++ covid19_game.py
@@ -4,9 +4,8 @@ import random
 import math
 
 # dimensions
-display_width = 1200
-display_height = 1200
-population = 75
+display_width = 1000
+display_height = 1000
 
 # color definitions
 black = (0, 0, 0)
@@ -23,7 +22,9 @@ yellow = (255, 255, 0)
 # global variables
 game_display = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
+population = 75
 virus_level = 0
+healthy_radius = 12
 game_exit = False
 people = []
 infected_people = []
@@ -53,14 +54,21 @@ class Vector:
 
 
 class Virus:
-    def __init__(self, level):
-        self.level = level
+    def __init__(self, duration, radius, color):
+        self.level = virus_level
         self.start_time = time.time()
-        self.duration = random.randrange(5 + int(level * 0.2), 6 + level)
-        self.radius = random.randrange(15 + int(level * 0.6), 16 + level)
-        self.color = (random.randrange(0, 255), 0,
-                      random.randrange(0, 255))
+        self.duration = duration
+        self.radius = radius
+        self.color = color
         # self.death_rate = death_rate
+
+    @classmethod
+    def mutate(cls):
+        return cls(
+            random.randrange(5 + int(virus_level * 0.2), 6 + virus_level),
+            random.randrange(healthy_radius + int(virus_level * 0.6), 16 + virus_level),
+            (random.randrange(0, 255), 0, random.randrange(0, 255))
+        )
 
     def get_time(self):
         return time.time() - self.start_time
@@ -98,9 +106,9 @@ class Virus:
 
 
 class Patient:
-    def __init__(self, pos, radius, healthy_color=green, virus=None):
+    def __init__(self, pos, healthy_color=green, virus=None):
         self.pos = pos
-        self.healthy_radius = radius
+        self.healthy_radius = healthy_radius
         self.radius = self.healthy_radius
         self.healthy_color = healthy_color
         self.immune = False
@@ -132,7 +140,11 @@ class Patient:
             return False
         for infected_patient in infected_people:
             if self.is_hit(infected_patient.pos, infected_patient.radius):
-                self.virus = infected_patient.virus
+                self.virus = Virus(
+                    infected_patient.virus.duration,
+                    infected_patient.virus.radius,
+                    infected_patient.virus.color
+                )
                 people.remove(self)
                 infected_people.append(self)
                 return True
@@ -162,8 +174,8 @@ class Patient:
 
 # TODO: NPC and Player inherits from the player superclass
 class Npc(Patient):
-    def __init__(self, healthy_radius, healthy_color=green, virus=None):
-        Patient.__init__(self, Vector(0, 0), healthy_radius, healthy_color, virus)
+    def __init__(self, healthy_color=green, virus=None):
+        Patient.__init__(self, Vector(0, 0), healthy_color, virus)
         self.pos = Vector(
             random.randrange(self.radius, display_width - self.radius),
             random.randrange(self.radius, display_height - self.radius)
@@ -189,10 +201,11 @@ class Npc(Patient):
 
 
 class Player(Patient):
-    def __init__(self, pos, healthy_radius, healthy_color, controls, virus=None):
-        Patient.__init__(self, pos, healthy_radius, healthy_color, virus)
+    def __init__(self, pos, healthy_color, controls, virus=None):
+        Patient.__init__(self, pos, healthy_color, virus)
         self.controls = controls
         self.start_time = time.time()
+        self.has_been_infected = False
 
     def animate(self):
         key_pressed = pygame.key.get_pressed()
@@ -206,22 +219,44 @@ class Player(Patient):
             self.move(0, 2)
         self.draw()
 
-    def time_survived(self):
-        if self.virus is None:
+    def is_game_over(self):
+        if not self.has_been_infected:
+            return
+        large_text = pygame.font.SysFont(None, 115)
+        text_surf, text_rect = text_objects("You were infected", large_text)
+        text_rect.center = (display_width/2, display_height/2)
+        game_display.blit(text_surf, text_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+            # game_display.fill(white)
+            '''button("Play Again", 150, 450, 100, 50, bright_green, green, game_loop)
+            button("Quit", 550, 450, 100, 50, bright_red, red, quit_game)'''
+
+            pygame.display.update()
+            clock.tick(120)
+
+    def display_score(self):
+        if self.virus is None and not self.has_been_infected:
             self.age = time.time() - self.start_time
+        else:
+            self.has_been_infected = True
         formatted_time = f"{self.age:.1f}"
         score_time = float(formatted_time)
         font = pygame.font.SysFont(None, 50)
-        text = font.render("Time uninfected: " + str(score_time), True, white)
-        game_display.blit(text, (10, 10))
-        return score_time
+        time_text = font.render("Time uninfected: " + str(score_time), True, white)
+        game_display.blit(time_text, (10, 10))
+        mutation_text = font.render("Mutation level: " + str(int(virus_level/10)), True, white)
+        game_display.blit(mutation_text, (10, 70))
 
 
 # functions
-def display_mutation_level():
-    font = pygame.font.SysFont(None, 50)
-    text = font.render("Mutation level: " + str(virus_level), True, white)
-    game_display.blit(text, (10, 70))
+def text_objects(text, font):
+    text_surface = font.render(text, True, white)
+    return text_surface, text_surface.get_rect()
 
 
 def quit_game():
@@ -241,19 +276,17 @@ def model_loop():
 
 
 def game_loop():
-    time.sleep(3)
     global virus_level
     global immune_people
     for i in range(0, population):
-        people.append(Npc(15))
+        people.append(Npc())
     player = Player(
         Vector(int(display_width/2), int(display_height/2)),
-        15,
         white,
         [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
     )
     people.append(player)
-    infected_people.append(Npc(15, virus=Virus(virus_level)))
+    infected_people.append(Npc(virus=Virus.mutate()))
 
     game_exit = False
     while not game_exit:
@@ -264,11 +297,11 @@ def game_loop():
         game_display.fill(black)
 
         if len(infected_people) == 0:
-            virus_level += 10
+            virus_level += 5
             for patient in immune_people:
                 patient.immune = False
                 if patient.virus_carrier:
-                    patient.virus = Virus(virus_level)
+                    patient.virus = Virus.mutate()
                     infected_people.append(patient)
                 else:
                     people.append(patient)
@@ -280,8 +313,8 @@ def game_loop():
             patient.animate()
         for patient in immune_people:
             patient.animate()
-        player.time_survived()
-        display_mutation_level()
+        player.display_score()
+        player.is_game_over()
         pygame.display.flip()
         clock.tick(120)
 
